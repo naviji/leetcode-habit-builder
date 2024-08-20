a03a29691bb6730975549bcb11bdec243f5b4fb5
Naveen M V <naveenmv7@gmail.com>
2024-08-20 18:41:12 +0530

    Transitions working

--- README.md
+++ README.md
@@ -2,11 +2,21 @@
 
 
 # TODO
+7. Redirect to intended page after completing problem
+8. Always detect completion
 0. Use requests that check if the redirect is happening to our problem url
     if so, save the url somewhere to redirect once problem is submitted successfully
 1. Add build step to put it into a dist folder
 2. Replace @types/chrome with chrome-types
 3. Add duolingo like addiction streaks
+4. Add option to not show already solved problems
+5. Add option to show me problems I have completed but did badly at?
+6. Change the number of questions that I need to complete
+7. Try reproducing and fix: it also recognizes that I solved the question no matter from which section of the problem i solved it from (i.e. solving the question from leetcode.com/problem/discussion doesn't trigger the extension)
+8. Choose your own question mode
+9. Fix the question vs redirection mismatch
+    The problem seems to be the midnight alarm, changing the problem state but not the ui?
+10. Add an exclusion list of websites
 
 # References
 https://developer.chrome.com/docs/extensions/develop
--- popup.ts
+++ popup.ts
@@ -1,54 +1,51 @@
-
-document.addEventListener('DOMContentLoaded', function() {
-    const settingsButton = document.getElementById('settings-icon');
-    if (settingsButton) {
-        settingsButton.addEventListener('click', function() {
-            // setRedirectRule('https://leetcode.com/problems/valid-anagram/');
-            const settingsPage = document.getElementById('settings-page');
-            if (settingsPage) {
-                settingsPage.style.display = 'block';
-            }
-          });
-    }
-
-    const backArrow = document.getElementById('arrow-icon');
-    if (backArrow) {
-        backArrow.addEventListener('click', function() {
-            const settingsPage = document.getElementById('settings-page');
-            if (settingsPage) {
-                settingsPage.style.display = 'none';
-            }
-          });
+const settingsButton = document.getElementById("settings-icon");
+if (settingsButton) {
+  settingsButton.addEventListener("click", function () {
+    // setRedirectRule('https://leetcode.com/problems/valid-anagram/');
+    const settingsPage = document.getElementById("settings-page");
+    if (settingsPage) {
+      settingsPage.classList.toggle("visible");
     }
   });
+}
+
+const backArrow = document.getElementById("arrow-icon");
+if (backArrow) {
+  backArrow.addEventListener("click", function () {
+    const settingsPage = document.getElementById("settings-page");
+    if (settingsPage) {
+      settingsPage.classList.toggle("visible");
+    }
+  });
+}
 
 function setRedirectRule(redirectUrl: string) {
-    console.log("Calling setRedirectRule")
-    const hostname = new URL(redirectUrl).hostname;
-    const redirectRule: chrome.declarativeNetRequest.Rule = {
-      id: 1,
-      action: {
-        type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
-        redirect: { url: redirectUrl }
-      },
-      condition: {
-        urlFilter: "*://*/*",
-        excludedInitiatorDomains: [
-          "developer.chrome.com", // To ease development
-          hostname
-        ],
-        resourceTypes: [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME]
-      }
+  console.log("Calling setRedirectRule");
+  const hostname = new URL(redirectUrl).hostname;
+  const redirectRule: chrome.declarativeNetRequest.Rule = {
+    id: 1,
+    action: {
+      type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
+      redirect: { url: redirectUrl }
+    },
+    condition: {
+      urlFilter: "*://*/*",
+      excludedInitiatorDomains: [
+        "developer.chrome.com", // To ease development
+        hostname
+      ],
+      resourceTypes: [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME]
     }
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [1],
-        addRules: [redirectRule]
-    })
+  };
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: [1],
+    addRules: [redirectRule]
+  });
 }
 
 function unsetRedirectRule() {
-    console.log("Calling unsetRedirectRule")
-    chrome.declarativeNetRequest.updateDynamicRules({
-        removeRuleIds: [1]
-    })
+  console.log("Calling unsetRedirectRule");
+  chrome.declarativeNetRequest.updateDynamicRules({
+    removeRuleIds: [1]
+  });
 }
