ffe37ed2e2361a2de238e988458ddf5f83bc8265
Naveen M V <naveenmv7@gmail.com>
2024-08-20 20:06:18 +0530

    Preserve page between sessions

--- popup.ts
+++ popup.ts
@@ -3,7 +3,14 @@ import { setRedirectRule, unsetRedirectRule } from './redirect.js'
 
 const settingsButton = document.getElementById("settings-icon");
 if (settingsButton) {
+  const storedState = localStorage.getItem('settingsPageVisible');
+  const settingsPage = document.getElementById("settings-page");
+  if (storedState === 'true' && settingsPage) {
+    settingsPage.classList.add("visible");
+  }
+
   settingsButton.addEventListener("click", function () {
+    localStorage.setItem('settingsPageVisible', 'true');
     const settingsPage = document.getElementById("settings-page");
     if (settingsPage) {
       settingsPage.classList.toggle("visible");
@@ -14,6 +21,7 @@ if (settingsButton) {
 const backArrow = document.getElementById("arrow-icon");
 if (backArrow) {
   backArrow.addEventListener("click", function () {
+    localStorage.setItem('settingsPageVisible', 'false');
     const settingsPage = document.getElementById("settings-page");
     if (settingsPage) {
       settingsPage.classList.toggle("visible");
