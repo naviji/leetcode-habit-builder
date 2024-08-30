4ad7b0117194d06958f0606e74f2beddd48fead9
Naveen M V <naveenmv7@gmail.com>
2024-08-30 16:20:08 +0530

    Add settings view toggle feature

--- scripts/popup.ts
+++ scripts/popup.ts
@@ -1,69 +1,103 @@
 import { Problem } from "./problems.js";
 
-const settingsButton = document.getElementById("settings-icon");
-if (settingsButton) {
-  const storedState = localStorage.getItem("settingsPageVisible");
-  const settingsPage = document.getElementById("settings-page");
-  if (storedState === "true" && settingsPage) {
-    settingsPage.classList.add("visible");
-  }
 
-  settingsButton.addEventListener("click", function () {
-    localStorage.setItem("settingsPageVisible", "true");
-    const settingsPage = document.getElementById("settings-page");
-    if (settingsPage) {
-      settingsPage.classList.toggle("visible");
-    }
-  });
-}
+document.addEventListener('DOMContentLoaded', () => {
+  const settingsPage = document.querySelector(".page__settings");
+  
+  if (settingsPage) {
+    // Retrieve the state from localStorage
+    const isOpen = localStorage.getItem("settingsPageOpen") === 'true';
 
-const backArrow = document.getElementById("arrow-icon");
-if (backArrow) {
-  backArrow.addEventListener("click", function () {
-    localStorage.setItem("settingsPageVisible", "false");
-    const settingsPage = document.getElementById("settings-page");
-    if (settingsPage) {
-      settingsPage.classList.toggle("visible");
-    }
-  });
-}
-
-const disableTorture = document.getElementById(
-  "disable-torture-checkbox",
-) as HTMLInputElement | null;
-if (disableTorture) {
-  const { disabled }: { disabled?: boolean } =
-    await chrome.storage.sync.get("disabled");
-  if (disabled) {
-    disableTorture.checked = disabled;
-  }
-
-  disableTorture.addEventListener("change", async function () {
-    await chrome.storage.sync.set({ disabled: !!disableTorture.checked });
-    if (disableTorture.checked) {
-      chrome.runtime.sendMessage({ action: "stopRedirect" });
+    // Set the initial state based on localStorage
+    if (isOpen) {
+      settingsPage.classList.add("isOpen");
     } else {
-      chrome.runtime.sendMessage({ action: "startRedirect" });
+      settingsPage.classList.remove("isOpen");
     }
-  });
-}
+  }
+})
 
-const solveBtn = document.getElementById("solve-btn") as HTMLDivElement | null;
-if (solveBtn) {
-  solveBtn.addEventListener("click", async function () {
-    const { problem }: { problem?: Problem } =
-      await chrome.storage.sync.get("problem");
-    if (problem) {
-      chrome.tabs.create({ url: problem.href });
-    }
-  });
-}
+const toggleSettings = () => {
+  const settingsPage = document.querySelector(".page__settings");
+  if (settingsPage) {
+    // Toggle the class
+    settingsPage.classList.toggle("isOpen");
 
-const problemTitle = document.getElementById("question");
-if (problemTitle) {
-  const { problem }: { problem?: Problem } =
-    await chrome.storage.sync.get("problem");
-  if (problem) {
-    problemTitle.textContent = problem.text;
+    // Save the state in localStorage
+    const isOpen = settingsPage.classList.contains("isOpen");
+    localStorage.setItem("settingsPageOpen", isOpen.toString());
   }
 }
+
+const settingsButton = document.querySelector(".header__icon_right");
+settingsButton?.addEventListener("click", toggleSettings)
+
+const backButton = document.querySelector(".header__icon_left");
+backButton?.addEventListener("click", toggleSettings)
+
+// if (settingsButton) {
+//   const storedState = localStorage.getItem("settingsPageVisible");
+//   const settingsPage = document.getElementById("settings-page");
+//   if (storedState === "true" && settingsPage) {
+//     settingsPage.classList.add("visible");
+//   }
+
+//   settingsButton.addEventListener("click", function () {
+//     localStorage.setItem("settingsPageVisible", "true");
+//     const settingsPage = document.getElementById("settings-page");
+//     if (settingsPage) {
+//       settingsPage.classList.toggle("visible");
+//     }
+//   });
+// }
+
+// const backArrow = document.getElementById("arrow-icon");
+// if (backArrow) {
+//   backArrow.addEventListener("click", function () {
+//     localStorage.setItem("settingsPageVisible", "false");
+//     const settingsPage = document.getElementById("settings-page");
+//     if (settingsPage) {
+//       settingsPage.classList.toggle("visible");
+//     }
+//   });
+// }
+
+// const disableTorture = document.getElementById(
+//   "disable-torture-checkbox",
+// ) as HTMLInputElement | null;
+// if (disableTorture) {
+//   const { disabled }: { disabled?: boolean } =
+//     await chrome.storage.sync.get("disabled");
+//   if (disabled) {
+//     disableTorture.checked = disabled;
+//   }
+
+//   disableTorture.addEventListener("change", async function () {
+//     await chrome.storage.sync.set({ disabled: !!disableTorture.checked });
+//     if (disableTorture.checked) {
+//       chrome.runtime.sendMessage({ action: "stopRedirect" });
+//     } else {
+//       chrome.runtime.sendMessage({ action: "startRedirect" });
+//     }
+//   });
+// }
+
+// const solveBtn = document.getElementById("solve-btn") as HTMLDivElement | null;
+// if (solveBtn) {
+//   solveBtn.addEventListener("click", async function () {
+//     const { problem }: { problem?: Problem } =
+//       await chrome.storage.sync.get("problem");
+//     if (problem) {
+//       chrome.tabs.create({ url: problem.href });
+//     }
+//   });
+// }
+
+// const problemTitle = document.getElementById("question");
+// if (problemTitle) {
+//   const { problem }: { problem?: Problem } =
+//     await chrome.storage.sync.get("problem");
+//   if (problem) {
+//     problemTitle.textContent = problem.text;
+//   }
+// }
