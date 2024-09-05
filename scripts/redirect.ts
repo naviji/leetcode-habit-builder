8afb1ffe48d900141609fe2fa86e75a3fe67d0bd
Naveen M V <naveenmv7@gmail.com>
2024-09-05 16:54:41 +0530

    Changed redirection to use window.replace()

--- scripts/redirect.ts
+++ scripts/redirect.ts
@@ -1,32 +1,50 @@
-let problemUrl = ""
 // let tablId: number | null = null
+let savedUrl: string | null = null
+let savedTabId: number | null = null
 
 const RULE_ID = 1
 
+const whitelistedUrls = [
+  'https://google.com',
+  'https://developer.chrome.com',
+  'https://leetcode.com',
+  // 'about:blank',
+  // 'about:srcdoc',
+  // 'https://www.recaptcha.net',
+  'chrome://new-tab-page/'
+];
+
+let redirectHandler: (details: chrome.webNavigation.WebNavigationParentedCallbackDetails) => void;
+
 export async function setRedirectRule(redirectUrl: string) {
   console.log("Setting Redirect", redirectUrl);
-  problemUrl = redirectUrl
-  console.log("problemUrl", problemUrl)
-  const hostname = new URL(redirectUrl).hostname;
-  const redirectRule: chrome.declarativeNetRequest.Rule = {
-    id: 1,
-    action: {
-      type: chrome.declarativeNetRequest.RuleActionType.REDIRECT,
-      redirect: { url: redirectUrl },
-    },
-    condition: {
-      urlFilter: "*://*/*",
-      excludedInitiatorDomains: [
-        "developer.chrome.com", // To ease development
-        hostname,
-      ],
-      resourceTypes: [chrome.declarativeNetRequest.ResourceType.MAIN_FRAME],
-    },
-  };
-  await chrome.declarativeNetRequest.updateDynamicRules({
-    removeRuleIds: [1],
-    addRules: [redirectRule],
-  });
+  // Function to check if the URL is whitelisted
+  function isUrlWhitelisted(currentUrl: string) {
+    return currentUrl.startsWith(redirectUrl) || whitelistedUrls.some(whitelistedUrl => currentUrl.startsWith(whitelistedUrl));
+  }
+
+  redirectHandler = ((details: chrome.webNavigation.WebNavigationParentedCallbackDetails) => {
+    console.log("details", details)
+    const currentUrl = details.url;
+
+    if (details.frameId !== 0 && details.parentFrameId != -1) {
+      return;
+    }
+
+    // If the current URL is not whitelisted, redirect the user
+    if (!isUrlWhitelisted(currentUrl)) {
+      console.log("Not whitelisted", currentUrl);
+      savedUrl = currentUrl;
+      savedTabId = details.tabId;
+      chrome.tabs.update(details.tabId, { url: redirectUrl });
+    } else {
+      console.log("Whitelisted", currentUrl);
+    }
+  })
+  // Listen for when a user navigates to a new page
+  chrome.webNavigation.onBeforeNavigate.addListener(redirectHandler);
+
+
   await chrome.scripting.unregisterContentScripts()
   await chrome.scripting.registerContentScripts([
     {
@@ -35,61 +53,16 @@ export async function setRedirectRule(redirectUrl: string) {
       matches: [`${redirectUrl}*`],
     }
   ])
-  chrome.webNavigation.onBeforeNavigate.addListener(handleBeforeNavigation);
-  chrome.webNavigation.onCommitted.addListener(handleOnCommitted);
 }
 
 export async function unsetRedirectRule() {
-  chrome.webNavigation.onCommitted.removeListener(handleOnCommitted);
-  chrome.webNavigation.onBeforeNavigate.removeListener(handleBeforeNavigation);
-
   await chrome.scripting.unregisterContentScripts()
   await chrome.declarativeNetRequest.updateDynamicRules({
     removeRuleIds: [RULE_ID],
   });
-  // problemUrl = ""
-  // const { urlTracker } = await chrome.storage.local.get("urlTracker")
-  // if (urlTracker) {
-  //   // Go throught each key value of object urlTracker
-  //   for (const [tabId, url] of Object.entries(urlTracker)) {
-  //     // If the value is not the same as the problem url then delete it
-  //     chrome.tabs.update(Number(tabId), { url: url as string })
-  //   }
-  //   await chrome.storage.local.remove("urlTracker")
-  // }
-  // console.log("urlTracker", urlTracker)
-}
-
-const handleBeforeNavigation = async (details: chrome.webNavigation.WebNavigationFramedCallbackDetails) => {
-  if (details.frameId === 0) {
-    // const { urlTracker = {} } = await chrome.storage.local.get("urlTracker")
-    // This is a main fram event navigation which could turn out to be redirected because of our ruleset
-    // Maintain this history on a per tab level for future redirect back
-    // urlTracker[details.tabId] = details.url
-    // chrome.storage.local.set({ urlTracker })
-
-    console.log("Going to url ", details.url, " from ", details.tabId)
-    // tablId = details.tabId
-  }
-}
-
-const handleOnCommitted = async (details: chrome.webNavigation.WebNavigationFramedCallbackDetails) => {
-  if (details.frameId === 0) {
-    // This is a main fram event
-      // if (tablId === details.tabId && details.url === problemUrl) {
-      //   console.log("Committed to url ", details.url, " from ", details.tabId)
-      //   tablId = null
-      // }
-
-    // const { urlTracker = {} } = await chrome.storage.local.get("urlTracker")
-
-    // if (urlTracker[details.tabId] && 
-    //   urlTracker[details.tabId] !== details.url) {
-    //     if (details.url !== problemUrl) {
-    //       // Some other redirection we don't care about; delete from our tracker
-    //       delete urlTracker[details.tabId]
-    //       await chrome.storage.local.set({ urlTracker })
-    //     }
-    // }
+  chrome.webNavigation.onBeforeNavigate.removeListener(redirectHandler)
+  console.log("Saved url and tablId", savedUrl, savedTabId);
+  if (savedUrl && savedTabId) {
+    chrome.tabs.update(savedTabId, { url: savedUrl });
   }
 }
